2b9986f8638aeec9e747be06edae88cd5a11500d
hugohp19 <hp19@hotmail.com>
2020-11-17 22:58:06 -0500

    get representative info form google civic api

--- server/controllers/representatives.js
+++ server/controllers/representatives.js
@@ -1,7 +1,18 @@
+const axios = require('axios');
 const Representatives = require('../db/models/representatives');
-import axios from 'axios';
 
 exports.getRepByZipcode = async (req, res) => {
   try {
-  } catch (error) {}
+    const city = req.body.city;
+    const { data } = await axios({
+      url: `https://www.googleapis.com/civicinfo/v2/representatives?key=${process.env.CIVIC_KEY}&address=${city}`,
+      method: 'GET'
+    });
+    const offices = data.offices;
+    const officials = data.officials;
+    res.status(202).json(officials);
+    console.log(offices);
+  } catch (error) {
+    console.log(error);
+  }
 };
