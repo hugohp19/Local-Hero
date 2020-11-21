cdedfe1e2244a30f3589c36baac298b097ab2810
purgatorygates <ulisesrivero1984@gmail.com>
2020-11-21 14:38:14 -0500

    connected registration to DB

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,32 @@
 import React, { useState } from 'react';
 import { Form, Button, Modal, Dropdown } from 'react-bootstrap';
+import axios from 'axios';
 
 const Login = () => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [formData, setFormData] = useState(null);
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+    console.log(formData);
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/login', formData);
+      console.log(response.data);
+      // sessionStorage.setItem('user', response.data);
+      // setCurrentUser(response.data.user);
+      // history.push('/');
+    } catch (error) {
+      console.log('SignUp Error: ', error.toString());
+    }
+  };
 
   return (
     <>
@@ -18,10 +39,15 @@ const Login = () => {
           <Modal.Title>Local Hero Login</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={handleLogin}>
             <Form.Group controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                onChange={handleChange}
+              />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -29,7 +55,12 @@ const Login = () => {
 
             <Form.Group controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={handleChange}
+              />
             </Form.Group>
             <Form.Group controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Remember me" />
@@ -39,7 +70,6 @@ const Login = () => {
             </Button>
           </Form>
         </Modal.Body>
-        <Modal.Footer></Modal.Footer>
       </Modal>
     </>
   );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Modal, Dropdown, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 const Register = () => {
   const [show, setShow] = useState(false);
@@ -7,6 +8,26 @@ const Register = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  // const { setCurrentUser } = useContext(AppContext);
+  const [formData, setFormData] = useState(null);
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+    console.log(formData);
+  };
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/signup', formData);
+      console.log(response.data);
+      // sessionStorage.setItem('user', response.data);
+      // setCurrentUser(response.data.user);
+      // history.push('/');
+    } catch (error) {
+      console.log('SignUp Error: ', error.toString());
+    }
+  };
   return (
     <>
       <Dropdown.Item variant="primary" onClick={handleShow}>
@@ -18,41 +39,87 @@ const Register = () => {
           <Modal.Title>Local Hero registration</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form>
-              <Form.Row>
-                <Col>
-                  <Form.Control placeholder="First name" />
-                </Col>
-                <Col>
-                  <Form.Control placeholder="Last name" />
-                </Col>
-              </Form.Row>
-            </Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+          <Form onSubmit={handleSignUp}>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridFirstName">
+                <Form.Control
+                  type="text"
+                  placeholder="Full name"
+                  name="name"
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridConfirmEmail">
+                <Form.Control
+                  type="email"
+                  placeholder="Confirm email"
+                  name="email"
+                  onChange={handleChange}
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridConfirmPassword">
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm Password"
+                  name="password"
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Group controlId="formGridAddress1">
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                placeholder="1234 Main St"
+                name="address"
+                onChange={handleChange}
+              />
             </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+
+            <Form.Group controlId="formGridAddress2">
+              <Form.Label>Address 2</Form.Label>
+              <Form.Control placeholder="Apartment, studio, or floor" />
             </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Remember me" />
-            </Form.Group>
+
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridCity">
+                <Form.Label>City</Form.Label>
+                <Form.Control />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label>State</Form.Label>
+                <Form.Control as="select" defaultValue="Choose...">
+                  <option>Choose...</option>
+                  <option>...</option>
+                </Form.Control>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridZip">
+                <Form.Label>Zip</Form.Label>
+                <Form.Control />
+              </Form.Group>
+            </Form.Row>
+
             <Button variant="primary" type="submit">
               Submit
             </Button>
           </Form>
         </Modal.Body>
-        <Modal.Footer></Modal.Footer>
       </Modal>
     </>
   );
